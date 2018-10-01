53c8aca921149ebe350fd2be873a2bd19712e79e
Jagdish <jagdish.ghimire1@gmail.com>
2018-09-30 21:57:20 -0500

    Created Empty Activities and also linked them to the Home Ativity.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+    public void openNumbersList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+
+    }
+
+    public void openFamilyList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+
+    }
+
+    public void openColorsList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+
+    }
+
+    public void openPhrasesList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+
     }
 }
